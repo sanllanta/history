212ac46f33d709e929f0476d71844928941eeb45
Andres F. Gomez D <andresfgomez86@gmail.com>
2014-10-14 11:59:09 -0500

    Image changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-6.8.9-Q16/"
