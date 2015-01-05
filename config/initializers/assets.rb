415c0297131519a024d82fadea1eb75608a12c5e
Kelvin <kelvin_guerrero@hotmail.com>
2015-01-05 11:36:37 -0500

    Cambios en la parte de edition del artwork

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( welcome.js )
 Rails.application.config.assets.precompile += %w( artwork_edit.js )
+Rails.application.config.assets.precompile += %w( template.js )
