5cfdfa45e80560a6357e2ebce392e3e300fa4284
Kelvin <kelvin_guerrero@hotmail.com>
2015-04-13 12:14:14 -0500

    Get full name for authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -8,4 +8,17 @@ class Author < ActiveRecord::Base
                                 },
                     :default_url => ":style/missing.png"
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
+
+  def get_full_name
+    if !name && !lastname
+      "N/A"
+    elsif !name
+      "#{lastname}"
+    elsif !lastname
+      "#{name}"
+    else
+      "#{lastname}, #{name}"
+    end
+  end
+
 end
