73da3360eade70027a2c734e247b10d697ce0ea3
Kelvin <kelvin_guerrero@hotmail.com>
2015-07-04 22:15:35 -0500

    Cambio en forma de vista de administración Cambio en la forma de creacion de usuario administrador parte 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, except:[:create, :new]
+  before_filter :authenticate_user!
 
   # GET /users
   # GET /users.json
@@ -63,13 +63,13 @@ class UsersController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
-      params.require(:user).permit(:email, :admin)
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+        params.require(:user).permit(:email, :admin)
+    end
 end
