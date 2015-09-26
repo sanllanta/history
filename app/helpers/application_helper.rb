414cd65a27344cfd37c6880af9d4d15057546f8a
xubuntu <sanllanta@gmail.com>
2015-09-26 13:17:52 -0500

    app_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,13 +9,19 @@ module ApplicationHelper
 
   def json_artworks_timeline(obras)
     j = ActiveSupport::JSON
-    json_map = obras.map do |u|
-      { :date => 'Feb. 18, 2003',
-        :display_date => '', :description => 'Brock Savelkoul joins the Army and is stationed at Fort Riley, Kan., in the artillery division.  His assigned position is artillery survey, meaning he prepares terrain for mounting artillery canon.',
-        :link => '',
+    json_map = obras.map do |o|
+      if o.creation_date
+      { :date => o.creation_date,
+        :display_date => '', 
+        :description => o.biographic_comment,
+        :link => url_for(o),
         :series => '',
-        :html => 'Brock Savelkoul',
-        :timestamp => 1045544400000 }
+        :html => 'HTML?',
+        :timestamp => o.creation_date }
+      elsif{
+
+      }
+      end
     end
     json = j.encode(json_map).to_s.html_safe
     p json
